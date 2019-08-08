340394d4ad3e1bb406c532115e022e3eef12fa06
harsh1395 <patelharsh1395@gmail.com>
2019-08-08 15:05:55 -0400

    after completing category

--- app/src/main/java/com/harsh/notesapp/NavigationDrawerAct.java
+++ app/src/main/java/com/harsh/notesapp/NavigationDrawerAct.java
@@ -1,6 +1,7 @@
 package com.harsh.notesapp;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -15,20 +16,81 @@ import android.view.MenuItem;
 import android.webkit.WebView;
 import android.widget.Toast;
 
+import DataBase.SqlHelperClass;
+
 
 public class NavigationDrawerAct extends AppCompatActivity{
 
     private WebView webView;
 
-
-
+    Toolbar customToolBar;
+    NavigationView nav_view;
+    AddNotes addNotes;
+    NotesList notesList;
+    DrawerLayout nav_drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_drawer);
+        nav_view =(NavigationView) findViewById(R.id.nav_view);
+       // this.setToolBar();
 
-///hello world
+
+        this.nav_drawer = findViewById(R.id.nav_drawer);
+        this.customToolBar = findViewById(R.id.Customtoolbar);
+        setSupportActionBar(customToolBar);
+        final ActionBar actionbar = getSupportActionBar();
+        actionbar.setTitle("My title");
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.open_navigation_drawer);
+
+        SqlHelperClass ob = new SqlHelperClass(NavigationDrawerAct.this);
+        SQLiteDatabase sqldb = (SQLiteDatabase) ob.getWritableDatabase();
+        this.addNotes = new AddNotes(this, sqldb);
+        this.notesList = new NotesList();
+
+        this.nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                FragmentTransaction ft = NavigationDrawerAct.this.getSupportFragmentManager().beginTransaction();
+                switch(menuItem.getItemId())
+                {
+                    case R.id.notes :
+
+                        ft.replace(R.id.frame_layout, notesList);
+                        ft.commit();
+
+                        break;
+
+                    case R.id.note_add :
+
+                        ft.replace(R.id.frame_layout, addNotes);
+                        ft.commit();
+
+
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.nav_drawer.openDrawer(Gravity.START);
+                Toast.makeText(NavigationDrawerAct.this, "onOptionsMethod" , Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
 
 }
